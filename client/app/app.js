5e5861a8def99f54596dbac54dde297c48d834a1
danielzhao <danielzhaozd@gmail.com>
2016-07-20 11:53:18 -0400

    appModule complete

--- client/app/app.js
+++ client/app/app.js
@@ -3,4 +3,26 @@ angular.module('app', [
   'app.services',
   'app.chat',
   'ngRoute'
-]);
+])
+.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .when('/login', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'AuthController'
+    })
+    .when('/chat', {
+      templateUrl: 'app/chat/chat.html',
+      controller: 'ChatController'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+});
